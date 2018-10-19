e16e5c1298b755aaa5615dd4e6305e7de7242973
smallfish666 <2581827655@qq.com>
2018-10-19 16:08:22 +0800

    new

--- model/usersmodel.js
+++ model/usersmodel.js
@@ -16,15 +16,48 @@ const usersModel = {
     add(data,cb){
 
         MongoClient.connect(url,function(err,client){
-            if (err) throw err;
+            if (err) {
+                console.log('连接数据库失败', err);
+                cb({ code: -100, msg: '连接数据库失败'});
+                return;
+            };
             const db = client.db('smallfish');
         
-            db.collection('users').insertOne(data,function(err){
-                if(err) throw err;
-                cb(null);
+            //1.  对 data 里面的 isAdmin 修改位 is_admin
+            //2.  写一个 ——id 为 1
+            //思考 ，下一个注册 ， 先得到之前的用户表的记录条数， +1 操作之后写给下一个注册的人。
+            //思考 ， 不容许用户名相同。
+
+            let saveDate = {
+                username: data.username,
+                password: data.password,
+                nickname: data.nickname,
+                phone: data.phone,
+                is_admin: data.isAdmin
+            };
+            db.collection('users').find({username: saveDate.username}).count(function(err,num){
+                //如果 num 为0 就是没有注册， 否则已经注册了
+                if (err) throw err;
+                if (num === 0) {
+                    
+                    db.collection('users').find().count(function(err, num){
+                        if (err) throw err;
+                        saveDate._id = num + 1;
+                        console.log(saveDate);
+                    
+        
+                    db.collection('users').insertOne(saveDate,function(err){
+                        if(err) throw err;
+                        cb(null);
+                        client.close();
+                    })
+                });
+                } else{
+                  cb('已经注册过了');
+                  client.close();
+                }
             })
 
-            client.close();
         })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const usersModel = require('../model/usersmodel.js');
+const usersModel = require('../model/usersModel.js');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -16,16 +16,34 @@ router.post('/register',function(req,res){
   console.log('获取传递过来的 post 请求的数据');
   console.log(req.body);
   //1.用户名必须是 5 - 10 的字符
-  if(!/w{5,10}/.test(req.body.username)){
+  if(/^w{5,10}$/.test(req.body.username)){
     res.render('werror',{ code: -1, msg: '用户名有误,请重新输入 (必须是 5-10 位)'});
+    return;
   }
   //操作数据库写入信息
 
-   usersModel.add(req.body,function(){
-     if (err) throw err;
-     //注册成功,跳到登录页面
-     res.sender('login');
-   });
+  //这里的捕获捕获不到， try catch 只能捕获同步的代码
+  // try{
+  //   usersModel.add(req.body,function(){
+  //     if (err) throw err;
+  //     //注册成功,跳到登录页面
+  //     res.render('login');
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.render('werror',{ code: -2, msg: error });
+
+  // }
+  //err需要时一个  对象  的格式{ code: 0,msg: 'xxx'}
+  usersModel.add(req.body,function(err){
+    if (err) {
+      //如果有错误，直接将错误信息渲染到页面
+      res.render('werror', err);
+    }else{
+      //注册成功
+      res.render('login');
+    }
+  })
 
 });
 module.exports = router;
